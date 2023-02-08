f6b68d24a79001463804388b84b55fb655cecacc
rdhpn <rdhgpn@gmail.com>
2023-02-08 11:32:23 -0500

    created barebone for fragment files

--- app/src/main/java/com/example/thecocktailsapp/view/DetailsFragment.kt
+++ app/src/main/java/com/example/thecocktailsapp/view/DetailsFragment.kt
@@ -2,11 +2,11 @@
 package com.example.thecocktailsapp.view
 
 
-import BaseFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.starwarsmvvm.utils.BaseFragment
 import com.example.thecocktailsapp.databinding.FragmentDetailsBinding
 
 class DetailsFragment : BaseFragment() {
