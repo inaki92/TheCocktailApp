0a93cf7c523ce68990fbb21d169394d431bc627f
rdhpn <rdhgpn@gmail.com>
2023-02-08 11:32:23 -0500

    created barebone for fragment files

---
+++ app/src/main/java/com/example/thecocktailsapp/utils/UIState.kt
@@ -0,0 +1,7 @@
+package com.example.starwarsmvvm.utils
+
+sealed class UIState<out T> {
+    object LOADING : UIState<Nothing>()
+    data class SUCCESS<T>(val response: T) : UIState<T>()
+    data class ERROR(val error: Exception) : UIState<Nothing>()
+}
