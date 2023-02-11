ca6f377509179ea3e54d520c9f78a2e2c1dde6b9
rdhpn <96323320+rdhpn@users.noreply.github.com>
2023-02-11 07:02:14 -0500

    Update CustomExceptions.kt

--- app/src/main/java/com/example/thecocktailsapp/utils/CustomExceptions.kt
+++ app/src/main/java/com/example/thecocktailsapp/utils/CustomExceptions.kt
@@ -1,4 +1,4 @@
-package com.example.thecocktailapp.utils
+package com.example.thecocktailsapp.utils
 
 class NullCocktailResponse(message: String = "Cocktail response is null") : Exception(message)
 class FailureResponse(message: String?) : Exception(message)
