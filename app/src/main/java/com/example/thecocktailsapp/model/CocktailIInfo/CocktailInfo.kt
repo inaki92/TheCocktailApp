8b82b42dc1fb1342707510fed4d8befbbd4a7456
carlosCabrera2 <carlos.cabrera@enhanceit.us>
2023-02-13 08:37:13 -0500

    Model Creation(fixes after pull request)  -removed the model duplicates  -implemented hilt in the gradle  CC

--- app/src/main/java/com/example/thecocktailsapp/model/CocktailIInfo/CocktailInfo.kt
+++ app/src/main/java/com/example/thecocktailsapp/model/CocktailIInfo/CocktailInfo.kt
@@ -4,8 +4,8 @@ package com.example.thecocktailsapp.model.CocktailIInfo
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
-data class CocktailInfo(
-    @Json(name = "drinks")
-    val drinks: List<DrinkXX>? = listOf()
+    @JsonClass(generateAdapter = true)
+    data class CocktailInfo
+    (@Json(name = "drinks")
+    val drinks: List<DrinkDetails>? = listOf()
 )
