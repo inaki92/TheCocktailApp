3b34481c16c20003848958c9b618d41938e695a6
rdhpn <rdhgpn@gmail.com>
2023-02-13 09:11:54 -0500

    updated references

--- app/src/main/java/com/example/thecocktailsapp/utils/CustomExceptions.kt
+++ app/src/main/java/com/example/thecocktailsapp/utils/CustomExceptions.kt
@@ -1,4 +1,3 @@
-package com.example.thecocktailsapp.utils
 
 class NullCocktailResponse(message: String = "Cocktail response is null") : Exception(message)
 class FailureResponse(message: String?) : Exception(message)
