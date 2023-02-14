809335adb483fd4706f877524d45ce9b60680b9f
luizeth0 <luis.hemi94@gmail.com>
2023-02-14 13:53:04 -0500

    Added the class CocktailsAppViewModel.kt that has a ViewModel instance from the androidx.lifecycle library

---
+++ app/src/main/java/com/example/thecocktailsapp/viewmodel/CocktailsAppViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.thecocktailsapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class CocktailsAppViewModel(): ViewModel() {
+
+    // Expose screen UI state
+
+
+    // Handle logic
+/*
+    private fun getCocktails(){}
+*/
+
+}
