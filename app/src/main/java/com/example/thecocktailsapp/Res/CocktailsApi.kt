ed97973924dcd683de653b288901b3ecb0220537
carlosCabrera2 <carlos.cabrera@enhanceit.us>
2023-02-14 13:44:05 -0500

    Rest Creation CC
    -created the CocktailsApi.kt and implements retrofit
    -created the CocktailsRepository.kt with implementation and interface.
    -refractored DrinkDetals to rename for CocktailsResponse.kt

--- app/src/main/java/com/example/thecocktailsapp/Res/CocktailsApi.kt
+++ app/src/main/java/com/example/thecocktailsapp/Res/CocktailsApi.kt
@@ -5,17 +5,39 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface CocktailsApi {
+interface CocktailsApi:  {
 
-    @GET(Cocktails)
-    suspend fun getCocktails(
+    @GET(cocktailsByName)
+    suspend fun getCocktailsByName(
         @Query("s") name: String
     ):Response<CocktailInfo>
+
+
+    @GET(cocktailsByAlcohol)
+        suspend fun getCocktailsByAlcohol(
+            @Query("a") name: String
+        ):Response<CocktailInfo>
+
+    @GET(cocktailsByRandom)
+        suspend fun getCocktailsByRandom(
+            @Query("i") name: String
+        ):Response<CocktailInfo>
+
+    @GET(cocktailsByRandomList)
+        suspend fun getCocktailsByRandomList(
+            @Query("i") name: String
+        ):Response<CocktailInfo>
+
+
+
 
 companion object {
 
         const val BASE_URL = "http://www.thecocktaildb.com/api/json/v1/1/"
-        private const val Cocktails = "search.php"
+        private const val cocktailsByName = "search.php"
+        private const val cocktailsByAlcohol = "filter.php"
+        private const val cocktailsByRandom = "lookup.php"
+        private const val cocktailsByRandomList = "lookup.php"
 
 
     }
--- app/src/main/java/com/example/thecocktailsapp/Res/CocktailsRepository.kt
+++ app/src/main/java/com/example/thecocktailsapp/Res/CocktailsRepository.kt
@@ -7,9 +7,22 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
+private const val TAG = "CocktailsRepository"
+
 interface CocktailsRepository{
-                fun getCocktails():
-                        Flow<UIState<List<CocktailsResponse>>>
+                fun getCocktailsByName():
+                        Flow<UIState<CocktailsResponse>>
+
+                fun getCocktailsByAlcohol():
+                        Flow<UIState<CocktailsResponse>>
+
+                fun getCocktailsByRandom():
+                        Flow<UIState<CocktailsResponse>>
+
+                fun getCocktailsByRandomList():
+                        Flow<UIState<CocktailsResponse>>
+
+
 }
 
 
@@ -17,7 +30,7 @@ class CocktailRepositoryImpl @Inject constructor(
             private val cocktailsApi: CocktailsApi
         ): CocktailsRepository{
 
-            override fun getCocktails(): Flow<UIState<List<CocktailsResponse>>> = flow{
+            override fun getCocktailsByName(): = flow{
                 emit(UIState.LOADING)
                 try {
                     val response = CocktailsAPI.getCocktails()
@@ -28,9 +41,56 @@ class CocktailRepositoryImpl @Inject constructor(
                     }else throw FailureResponseException(response.errorBody()?.string())
                 }catch (e: Exception){
                     emit(UIState.ERROR(e))
-                    Log.e(TAG, "getCocktails: ${e.localizedMessage}", e)
+                    Log.e(TAG, "getCocktailsByName: ${e.localizedMessage}", e)
                 }
             }
+
+        override fun getCocktailsByAlcohol(): Flow<UIState<List<CocktailsResponse>>> = flow{
+            emit(UIState.LOADING)
+            try {
+                val response = CocktailsAPI.getCocktails()
+                if(response.isSuccessful){
+                    response.body()?.let {
+                        emit(UIState.SUCCESS(it))
+                    }?: throw NullCocktailsException() //check if response was null
+                }else throw FailureResponseException(response.errorBody()?.string())
+            }catch (e: Exception){
+                emit(UIState.ERROR(e))
+                Log.e(TAG, "getCocktailsByAlcohol: ${e.localizedMessage}", e)
+            }
+        }
+
+
+        override fun getCocktailsByRandom(): Flow<UIState<List<CocktailsResponse>>> = flow{
+            emit(UIState.LOADING)
+            try {
+                val response = CocktailsAPI.getCocktails()
+                if(response.isSuccessful){
+                    response.body()?.let {
+                        emit(UIState.SUCCESS(it))
+                    }?: throw NullCocktailsException() //check if response was null
+                }else throw FailureResponseException(response.errorBody()?.string())
+            }catch (e: Exception){
+                emit(UIState.ERROR(e))
+                Log.e(TAG, "getCocktailsByRandom: ${e.localizedMessage}", e)
+            }
+        }
+
+        override fun getCocktailsByRandomList(): Flow<UIState<List<CocktailsResponse>>> = flow{
+            emit(UIState.LOADING)
+            try {
+                val response = CocktailsAPI.getCocktails()
+                if(response.isSuccessful){
+                    response.body()?.let {
+                        emit(UIState.SUCCESS(it))
+                    }?: throw NullCocktailsException() //check if response was null
+                }else throw FailureResponseException(response.errorBody()?.string())
+            }catch (e: Exception){
+                emit(UIState.ERROR(e))
+                Log.e(TAG, "getCocktailsByRandomList: ${e.localizedMessage}", e)
+            }
+        }
+
 
 
         }
