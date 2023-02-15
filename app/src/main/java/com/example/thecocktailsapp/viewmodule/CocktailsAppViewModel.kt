ac3ba4b749e823898eb8bc4a630c8d7edbc77239
luizeth0 <luis.hemi94@gmail.com>
2023-02-15 13:23:47 -0500

    Added the fun getCocktails

--- app/src/main/java/com/example/thecocktailsapp/viewmodule/CocktailsAppViewModel.kt
+++ app/src/main/java/com/example/thecocktailsapp/viewmodule/CocktailsAppViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.thecocktailsapp.viewmodule
 
+import android.content.ContentValues
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.thecocktailsapp.model.CocktailIInfo.CocktailsResponse
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
-class CocktailsAppViewModel(): ViewModel() {
+class CocktailsAppViewModel(
+    private val cocktailsRepository: CocktailsRepository
+): ViewModel() {
 
     // Expose screen UI state
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val cocktails = ""
 
     private val _cocktail : MutableLiveData<UIState<CocktailsResponse>> = MutableLiveData(UIState.LOADING)
     val cocktail : MutableLiveData<UIState<CocktailsResponse>> get() = _cocktail
@@ -17,8 +22,12 @@ class CocktailsAppViewModel(): ViewModel() {
 
     // Handle logic
     private fun getCocktails(){
-
-
+        viewModelScope.launch(ioDispatcher) {
+            cocktailsRepository.getCocktail(cocktails).collect() {
+                Log.d(ContentValues.TAG, "TEST: $cocktails")
+                _cocktail.postValue(it)
+            }
+        }
 
     }
 
