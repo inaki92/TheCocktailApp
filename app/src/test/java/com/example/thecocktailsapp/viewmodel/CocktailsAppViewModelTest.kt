60ed93671d8635e46a9beb753e735ee4343110d3
luizeth0 <luis.hemi94@gmail.com>
2023-02-16 10:17:04 -0500

    Created Test for ViewModel

---
+++ app/src/test/java/com/example/thecocktailsapp/viewmodel/CocktailsAppViewModelTest.kt
@@ -0,0 +1,17 @@
+package com.example.thecocktailsapp.viewmodel
+
+import org.junit.Assert.*
+
+import org.junit.After
+import org.junit.Before
+
+class CocktailsAppViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+}
