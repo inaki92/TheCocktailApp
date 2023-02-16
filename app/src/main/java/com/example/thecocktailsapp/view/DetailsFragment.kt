e8e1567e632ee74a337fb970e9ebf792a3ebda12
rdhpn <rdhgpn@gmail.com>
2023-02-16 10:32:30 -0500

    Fixed Fragment Files

--- app/src/main/java/com/example/thecocktailsapp/view/DetailsFragment.kt
+++ app/src/main/java/com/example/thecocktailsapp/view/DetailsFragment.kt
@@ -2,12 +2,12 @@
 package com.example.thecocktailsapp.view
 
 
-import BaseFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.thecocktailsapp.databinding.CocktailsDetailsFragmentBinding
+import com.example.thecocktailsapp.utils.BaseFragment
 
 class DetailsFragment : BaseFragment() {
 
